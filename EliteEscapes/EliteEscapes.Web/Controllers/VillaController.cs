166b215cac1d322271f28c4dbe05266d59e5b961
Fresil Patel <technicalfresil@gmail.com>
2025-03-11 17:20:29 +0530

    11-03 3hr changes

---
+++ EliteEscapes/EliteEscapes.Web/Controllers/VillaController.cs
@@ -0,0 +1,37 @@
+﻿using EliteEscapes.Domain.Entities;
+using EliteEscapes.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EliteEscapes.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var Villas = _context.Villas.ToList();
+            return View(Villas);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                _context.Villas.Add(obj);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
