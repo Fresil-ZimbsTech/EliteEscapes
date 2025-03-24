f15d188d3c5315f3a33591dda59ae8aaaeb89950
Fresil Patel <technicalfresil@gmail.com>
2025-03-24 17:21:57 +0530

    Add Dashboard and Charts

--- EliteEscapes/EliteEscapes.Web/Controllers/DashboardController.cs
+++ EliteEscapes/EliteEscapes.Web/Controllers/DashboardController.cs
@@ -1,12 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EliteEscapes.Application.Common.Interfaces;
+using EliteEscapes.Application.Common.Utility;
+using EliteEscapes.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EliteEscapes.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        static int previousMonth = DateTime.Now.Month == 1 ? 12 : DateTime.Now.Month - 1;
+        readonly DateTime previousMonthStartDate = new(DateTime.Now.Year, previousMonth, 1);
+        readonly DateTime currentMonthStartDate = new(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+        public DashboardController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public async Task<IActionResult> GetTotalBookingRadialChartData()
+        {
+            var totalbookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending || u.Status == SD.StatusCancelled);
+
+            var countByCurrentMonth = totalbookings.Count(u => u.BookingDate >= currentMonthStartDate && u.BookingDate <= DateTime.Now);
+
+            var countByPreviousMonth = totalbookings.Count(u => u.BookingDate >= previousMonthStartDate && u.BookingDate <= currentMonthStartDate);
+
+            return Json(GetRadialCartDataModel(totalbookings.Count(), countByCurrentMonth, countByPreviousMonth));
+        }
+
+
+        public async Task<IActionResult> GetRegisteredUserChartData()
+        {
+            var totalUsers = _unitOfWork.User.GetAll(); 
+             
+            var countByCurrentMonth =totalUsers.Count(u=>u.CreatedAt >= currentMonthStartDate && u.CreatedAt <= DateTime.Now);
+
+            var countByPreviousMonth = totalUsers.Count(u => u.CreatedAt >= previousMonthStartDate && u.CreatedAt <= currentMonthStartDate);
+
+            return Json(GetRadialCartDataModel(totalUsers.Count(), countByCurrentMonth, countByPreviousMonth));
+        }
+
+        public async Task<IActionResult> GetRevenueChartData()
+        {
+            var totalBookings = _unitOfWork.Booking.GetAll(u => u.Status != SD.StatusPending
+           || u.Status == SD.StatusCancelled);
+
+            var totalRevenue = Convert.ToInt32(totalBookings.Sum(u => u.TotalCost));
+
+            var countByCurrentMonth = totalBookings.Where(u => u.BookingDate >= currentMonthStartDate &&
+            u.BookingDate <= DateTime.Now).Sum(u => u.TotalCost);
+
+            var countByPreviousMonth = totalBookings.Where(u => u.BookingDate >= previousMonthStartDate &&
+            u.BookingDate <= currentMonthStartDate).Sum(u => u.TotalCost);
+
+            return Json(GetRadialCartDataModel(totalRevenue, countByCurrentMonth, countByPreviousMonth));
+        }
+
+
+        private static RadialBarChartVM GetRadialCartDataModel(int totalCount, double currentMonthCount, double prevMonthCount)
+        {
+            RadialBarChartVM radialBarChartVM = new();
+
+            int increaseDecreaseRatio = 100;
+
+            if (prevMonthCount != 0)
+            {
+                increaseDecreaseRatio = Convert.ToInt32((currentMonthCount - prevMonthCount) / prevMonthCount * 100);
+            }
+            radialBarChartVM.TotalCount = totalCount;
+            radialBarChartVM.CountInCurrentMonth = Convert.ToInt32(currentMonthCount);
+            radialBarChartVM.HasRatioIncreased = currentMonthCount > prevMonthCount;
+            radialBarChartVM.Series = new int[] { increaseDecreaseRatio };
+
+            return radialBarChartVM;
+
+        }
+
+
     }
 }
+
