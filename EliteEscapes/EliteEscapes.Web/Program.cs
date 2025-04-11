71f3546abc4e18f494d6e3a71be2c4524c74feff
Fresil Patel <technicalfresil@gmail.com>
2025-04-11 17:05:44 +0530

    temp push

--- EliteEscapes/EliteEscapes.Web/Program.cs
+++ EliteEscapes/EliteEscapes.Web/Program.cs
@@ -20,7 +20,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders(); // ? <-- Fixes token generation for reset password
+
 
 //COOKIE CONFIGURATION 
 builder.Services.ConfigureApplicationCookie(option =>
